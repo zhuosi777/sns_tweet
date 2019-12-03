006ed661949cdd8f0a8f85b0405c40d8b038bea3
zhuosi777 <zhuosi777@yahoo.co.jp>
2019-12-03 15:09:42 +0900

    createルート作成

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,8 +1,13 @@
 class TweetsController < ApplicationController
 
   def index
+    @tweets = Tweet.all
   end
 
   def new
+  end
+
+  def create
+      Tweet.create(name: "", image: "", text: "")
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   
 resources :tweets, only:[:index, :new]
+  collection do
+    post 'create', to: 'tweets#create'
 
 end
