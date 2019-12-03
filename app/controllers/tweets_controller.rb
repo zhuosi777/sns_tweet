c0d4fb917bdb9c57894c009230b33d8c60a97106
zhuosi777 <zhuosi777@yahoo.co.jp>
2019-12-03 16:06:08 +0900

    コントローラー修正

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,5 +1,7 @@
 class TweetsController < ApplicationController
 
+  before_action :move_to_index, except: :index
+
   def index
     @tweets = Tweet.order("created_at DESC").page(params[:page]).per(5)
   end
@@ -14,5 +16,9 @@ class TweetsController < ApplicationController
   private
   def tweet_params
     params.permit(:name, :image, :text)
+  end
+
+  def move_to_index
+    redirect_to action: :index unless user_signed_in?
   end
 end
