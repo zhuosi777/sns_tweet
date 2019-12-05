1fb0d758cad7d0bfd250b5a92d88cdb2946cc3a9
zhuosi777 <zhuosi777@yahoo.co.jp>
2019-12-05 12:22:32 +0900

    resousece設定

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,5 @@ Rails.application.routes.draw do
   devise_for :users
   root "tweets#index"
   resources :tweets
-  get   'users/:id'   =>  'users#show'
-  patch   'tweets/:id'  => 'tweets#update'
+  resources :users, only: [:show]
 end
