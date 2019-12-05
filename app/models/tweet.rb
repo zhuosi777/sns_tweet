98ecc22e4dba0f4ee4bb8f344aae76c368896f7d
zhuosi777 <zhuosi777@yahoo.co.jp>
2019-12-05 12:21:15 +0900

    コメントテーブル作成とアソシエーション組み実行

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,5 +1,5 @@
 class Tweet < ApplicationRecord
 
   belongs_to :user
-
+  has_manuy :comments
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_03_091441) do
+ActiveRecord::Schema.define(version: 2019_12_05_031702) do
+
+  create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "tweet_id"
+    t.text "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "tweets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "text"
